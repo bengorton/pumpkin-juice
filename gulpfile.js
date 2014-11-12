7cbfc3e702cf6c5d3a76fdc3ebab5f14aed5c4c2
ben <ben@bens-MacBook-Pro.local>
2014-11-12 00:27:34 -0800

    stylus and jade working, putting together defaults for stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,28 @@
-//initialize all of our variables
-var app, base, to5, concat, directory, gulp, gutil, hostname, path, refresh, sass, uglify, imagemin, cache, minifyCSS, del, harp;
+'use strict'
 
-//load all of our dependencies
-//add more here if you want to include more libraries
-gulp        = require('gulp');
-to5         = require('gulp-6to5');
-gutil       = require('gulp-util');
-concat      = require('gulp-concat');
-uglify      = require('gulp-uglify');
+var gulp      = require('gulp'),
+  to5         = require('gulp-6to5'),
+  gutil       = require('gulp-util'),
+  concat      = require('gulp-concat'),
+  uglify      = require('gulp-uglify'),
 
-stylus      = require('gulp-stylus');
-jeet        = require('jeet');
-rupture     = require('rupture');
-minifyCSS   = require('gulp-minify-css');
+  stylus      = require('gulp-stylus'),
+  jeet        = require('jeet'),
+  nib         = require('nib'),
+  rupture     = require('rupture'),
+  minifyCSS   = require('gulp-minify-css'),
 
-jade        = require('gulp-jade');
-imagemin    = require('gulp-imagemin');
-cache       = require('gulp-cache');
-harp        = require('harp');
-del         = require('del');
+  jade        = require('gulp-jade'),
+  imagemin    = require('gulp-imagemin'),
+  cache       = require('gulp-cache'),
+  harp        = require('harp'),
+  del         = require('del');
 
 gulp.task('serve', function () {
   harp.server(__dirname + '/public', {
     port: 9000
   });
 });
-
 
 //compressing images & handle SVG files
 gulp.task('optimize-images', function(tmp) {
@@ -34,43 +31,18 @@ gulp.task('optimize-images', function(tmp) {
     .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
 });
 
-//moving images to public
-gulp.task('images-deploy', function() {
-  gulp.src(['app/assets/images/**/*'])
-    .pipe(gulp.dest('public/images'))
-});
-
-gulp.task('fonts', function() {
-  gulp.src(['app/assets/fonts/**/*'])
-    .pipe(gulp.dest('public/fonts'))
-});
-
+/**
+ * jade compiler
+ */
 gulp.task('templates', function() {
-  gulp.src('./app/**/*.jade')
-    .pipe(jade())
-    .pipe(gulp.dest('/public'))
+  gulp.src(['./app/*.jade', './app/layout/**/*.jade'])
+    .pipe(jade({pretty:true}))
+    .pipe(gulp.dest('public'))
 });
 
-gulp.task('assets', ['templates', 'fonts', 'images-deploy']);
-
-//migrating over all HTML files for deployment
-gulp.task('html-deploy', function() {
-    //grab everything, which should include htaccess, robots, etc
-    gulp.src('app/*')
-        .pipe(gulp.dest('public'));
-
-    //grab any hidden files too
-    gulp.src('app/.*')
-        .pipe(gulp.dest('public'));
-
-    gulp.src('app/fonts/**/*')
-        .pipe(gulp.dest('public/fonts'));
-
-    //grab all of the styles
-    gulp.src(['app/styles/*.css', '!app/styles/styles.css'])
-        .pipe(gulp.dest('public/styles'));
-});
-
+/**
+ * es6 compile and concat
+ */
 gulp.task('scripts', function () {
   return gulp.src('app/js/**/*.js')
     .pipe(to5())
@@ -87,9 +59,13 @@ gulp.task('scripts-deploy', function () {
     .pipe(gulp.dest('public/js'))
 });
 
+/**
+ * stylus compliation, deploy version
+ * compresses
+ */
 gulp.task('styles', function() {
   return gulp.src('app/styles/app.styl')
-    .pipe(stylus({use: [jeet()], compress: false}))
+    .pipe(stylus({use: [jeet(), nib()], compress: false}))
     .pipe(gulp.dest('public/styles'))
 });
 
@@ -102,9 +78,23 @@ gulp.task('styles-deploy', function() {
     .pipe(gulp.dest('public/styles'))
 });
 
-//cleans our public directory in case things got deleted
+
+/**
+ * clean up public
+ */
 gulp.task('clean', function() {
     del('public');
+});
+
+/**
+ * run all the assets tasks
+ */
+gulp.task('assets', ['templates'], function() {
+  gulp.src(['app/assets/fonts/**/*'])
+    .pipe(gulp.dest('public/fonts'))
+
+  gulp.src(['app/assets/images/**/*'])
+    .pipe(gulp.dest('public/images'))
 });
 
 /**
@@ -114,9 +104,9 @@ gulp.task('clean', function() {
  */
 gulp.task('default', ['scripts', 'styles', 'templates', 'assets'], function() {
     //a list of watchers, so it will watch all of the following files waiting for changes
-    gulp.watch('app/scripts/src/**', ['scripts']);
-    gulp.watch('app/styles/scss/**', ['styles']);
-    gulp.watch('app/images/**', ['images']);
+    gulp.watch('app/**/*.js', ['scripts']);
+    gulp.watch('app/styles/**/*', ['styles']);
+    gulp.watch('app/images/**/*', ['images']);
     gulp.watch('app/**/*.jade', ['templates']);
 });
 
